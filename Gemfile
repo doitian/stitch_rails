3b0179a8877238c50ead7da60a1bb3629a553609
Ian Yang <me@iany.me>
2012-02-18 15:15:57 +0800

    initial

---
+++ Gemfile
@@ -0,0 +1,6 @@
+source "http://ruby.taobao.org"
+
+group :development do
+  gem "bundler", "~> 1.0.0"
+  gem "jeweler", "~> 1.8.3"
+end
---
+++ lib/stitch_rails.rb
@@ -0,0 +1,34 @@
+module Stitch
+  class StitchyCoffeeScriptTemplate < Tilt::Template
+    self.default_mime_type = 'application/javascript'
+
+    def self.engine_initialized?
+      defined? ::CoffeeScript
+    end
+
+    def initialize_engine
+      require_template_library 'coffee_script'
+    end
+
+    def prepare
+      options[:bare] = true
+    end
+
+    def evaluate(scope, locals, &block)
+      @output ||= "\nrequire.define({'#{ module_name(scope) }': function(exports, require, module) {\n" + CoffeeScript.compile(data, options) + "\n}});\n"
+    end
+
+    private
+    # this might need to be customisable to generate the desired module names
+    # this implementation lops off the first segment of the path
+    def module_name(scope)
+      scope.logical_path
+    end
+  end
+
+  class Railtie < Rails::Railtie
+    initializer 'stitch.configure_rails_initialization' do |app|
+      app.assets.register_engine '.coffee', StitchyCoffeeScriptTemplate
+    end
+  end
+end
